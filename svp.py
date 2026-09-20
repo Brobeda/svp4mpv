b4e851184716d673cf6b577164e45198d64f4c3e
xrun1 <126838916+xrun1@users.noreply.github.com>
2026-09-20 09:23:33 +0200

    Better naming for some svp.py variables

--- svp.py
+++ svp.py
@@ -43,76 +43,76 @@ win_w, win_h = display_res
 # win_w, win_h = user_data.split("/")
 # win_w, win_h = int(win_w), int(win_h)
 
-options: dict[str, str] = json.loads(config_json.read_text())
-cmap: ConfigMap = json.loads((basedir / "map.json").read_text())
+user_cfg: dict[str, str] = json.loads(config_json.read_text())
+cfg2svparams: ConfigMap = json.loads((basedir / "map.json").read_text())
 
-raw: dict[str, dict[str, Any]] = {}
-for _section, opts in cmap.items():  # noqa: PERF102
+svparams: dict[str, dict[str, Any]] = {}
+for _section, opts in cfg2svparams.items():  # noqa: PERF102
     for name, choices in opts.items():
-        if (choice := options.get(snake_case(name))):
-            deep_merge(choices[choice], raw)
+        if (choice := user_cfg.get(snake_case(name))):
+            deep_merge(choices[choice], svparams)
 
-if options["fill_with_light"] == "Disabled":
-    raw["smoothfps"]["light"] = {"lights": 2, "length": 0, "aspect": 1.7778}
+if user_cfg["fill_with_light"] == "Disabled":
+    svparams["smoothfps"]["light"] = {"lights": 2, "length": 0, "aspect": 1.7778}
 
 src_fps = cast("float", container_fps)
 if src_fps <= 0.1 or round(src_fps, 2) == 23.81:
     src_fps = 23.976
 
-fa = options["multiplicand"]
-fb = options["multiplier"]
+base = user_cfg["multiplicand"]
+times = user_cfg["multiplier"]
 to_fps = src_fps
 screen_fps = cast("float", display_fps) or 60
 
-if fa == "Video FPS":
-    if str(fb).startswith("Auto"):
+if base == "Video FPS":
+    if str(times).startswith("Auto"):
         factor = 1
         while src_fps * factor < screen_fps - 9:
             factor += 1
 
         to_fps = src_fps * factor
-        if fb == "Auto (respect vsync)":
+        if times == "Auto (respect vsync)":
             to_fps = min(to_fps, screen_fps)
     else:
-        to_fps = src_fps * float(fb)
-elif fa == "Screen FPS":
-    if str(fb).startswith("Auto"):
-        fb = "1"
-    to_fps = screen_fps * float(fb)
+        to_fps = src_fps * float(times)
+elif base == "Screen FPS":
+    if str(times).startswith("Auto"):
+        times = "1"
+    to_fps = screen_fps * float(times)
 else:
-    if str(fb).startswith("Auto"):
-        fb = "1"
-    to_fps = float(fa.split(" FPS")[0]) * float(fb)
+    if str(times).startswith("Auto"):
+        times = "1"
+    to_fps = float(base.split(" FPS")[0]) * float(times)
 
-raw["smoothfps"].setdefault("rate", {}).update({
+svparams["smoothfps"].setdefault("rate", {}).update({
     "num": to_fps * 10_000,
     "den": 10_000,
     "abs": True,
 })
-raw["smoothfps"].setdefault("light", {})["aspect"] = win_w / (win_h or 1)
+svparams["smoothfps"].setdefault("light", {})["aspect"] = win_w / (win_h or 1)
 # TODO: light settings, NVOF, RIFE, 8/10bit options
 
 deep_merge({
-    "super": json.loads(options["json_super"] or "{}"),
-    "analyse": json.loads(options["json_analyse"] or "{}"),
-    "smoothfps": json.loads(options["json_smoothfps"] or "{}"),
-}, raw)
+    "super": json.loads(user_cfg["json_super"] or "{}"),
+    "analyse": json.loads(user_cfg["json_analyse"] or "{}"),
+    "smoothfps": json.loads(user_cfg["json_smoothfps"] or "{}"),
+}, svparams)
 
 # [(categoryName, [(optionName, [value, ...]), ...]), ...]
-menu_options: list[tuple[str, list[tuple[str, list[str]]]]] = []
-for section, stuff in cmap.items():
+menu_entries: list[tuple[str, list[tuple[str, list[str]]]]] = []
+for section, stuff in cfg2svparams.items():
     if section == "Overrides":
         continue
     opts = [(opt, list(choices)) for opt, choices in stuff.items()]
-    menu_options.append((section, opts))
+    menu_entries.append((section, opts))
 
-menu_json.write_text(json.dumps(menu_options, indent=4))
+menu_json.write_text(json.dumps(menu_entries, indent=4))
 
 core = vs.core
 core.num_threads = ((os.cpu_count() or 2) * 2) - 1
 core.max_cache_size = 8192
 
-thread_opt = options["processing_threads"]
+thread_opt = user_cfg["processing_threads"]
 if thread_opt != "Do not change":
     core.num_threads += int(thread_opt)
 
@@ -121,7 +121,7 @@ if not hasattr(core, "svp1"):
 if not hasattr(core, "svp2"):
     core.std.LoadPlugin(basedir / "third_party" / "svpflow2_vs.dll")
 
-if options["duplicate_frames_removal"] == "Remove every other frame":
+if user_cfg["duplicate_frames_removal"] == "Remove every other frame":
     clip = video_in.std.SelectEvery(video_in, 2, 0).std.Trim(length=5000000)
 else:
     clip = video_in.std.Trim(length=5000000)
@@ -137,16 +137,16 @@ else:  # no 10 bit decoding
     input_m8 = input_m
 
 
-sup = core.svp1.Super(input_m8, json.dumps(raw["super"]))
+sup = core.svp1.Super(input_m8, json.dumps(svparams["super"]))
 vectors = core.svp1.Analyse(
-    sup["clip"], sup["data"], input_m8, json.dumps(raw["analyse"]),
+    sup["clip"], sup["data"], input_m8, json.dumps(svparams["analyse"]),
 )
 smooth = core.svp2.SmoothFps(
     input_m, sup["clip"], sup["data"], vectors["clip"], vectors["data"],
-    json.dumps(raw["smoothfps"]), src=input_um, fps=src_fps,
+    json.dumps(svparams["smoothfps"]), src=input_um, fps=src_fps,
 )
 
-if options["fill_with_light"] == "Disabled":
+if user_cfg["fill_with_light"] == "Disabled":
     delta_w = smooth.width - clip.width
     delta_h = smooth.height - clip.height
     if delta_w or delta_h:
